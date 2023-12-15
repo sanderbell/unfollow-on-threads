233967fb89e9641f6aea7957254d975094d5057e
Sander Bell <122751409+sanderbell@users.noreply.github.com>
2023-12-15 11:29:15 +0700

    Added comments to script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Find all focusable `div` elements (except those with `tabindex="-1"`)
 const buttons = document.querySelectorAll('div[tabindex]:not([tabindex="-1"])');
+// Loop through each found button
 for (let button of buttons) {
+  // Check if the button's text contains "Following"
   if (button.textContent.includes('Following')) {
+    // Click the button
     button.click();
+    // Wait for 500 milliseconds (simulated delay)
     await new Promise((resolve) => setTimeout(resolve, 500));
   }
 }
